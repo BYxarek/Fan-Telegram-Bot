913b70d9afbfc2de1dae6f82da30a663f59ac079
BYxarek <broekitin@gmail.com>
2024-12-30 21:58:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ TOKEN = '7635241640:AAHodi2PDqvQV_fVMRsJ--yH6dkOGRfWZ-s'
 bot = telebot.TeleBot(TOKEN)
 
 # Список случайных фраз
+phrases_hello = [
+    "Добро пожаловать, {user}!",
+    "Привет, {user}!",
+    "Рады видеть тебя, {user}!",
+    "Приветствуем, {user}!",
+    "Добро пожаловать в наш чат, {user}!"
+]
+
 phrases_byby = [
     "Пользователь {user} покинул нас. Прощай!",
     "Прощай, {user}!",
@@ -13,10 +21,19 @@ phrases_byby = [
     "Удачи, {user}!"
 ]
 
-@bot.message_handler(commands=['start'])
+phrases_thankful = [
+    "Спасибо, что добавили меня в чат! Мой исходный код есть на GitHub (не забудь подписаться и поставить звезду) - https://github.com/BYxarek/Fan-Telegram-Bot"
+]
+
+@bot.message_handler(commands=['start', 'why'])
 def send_welcome(message):
     if message.chat.type == 'private':
         bot.reply_to(message, "Извините, я работаю только в группах.")
+    elif message.text == "/why":
+        bot.reply_to(message, "Функции бота:\n"
+                             "1. Приветствие новых пользователей.\n"
+                             "2. Прощание с пользователями, покидающими чат.\n"
+                             "3. Благодарность при добавлении бота в чат.")
 
 # Обработчик исключения пользователя
 @bot.message_handler(content_types=['left_chat_member'])
@@ -25,15 +42,26 @@ def handle_left_member(message):
     phrase = random.choice(phrases_byby).format(user=user)
     bot.send_message(message.chat.id, phrase)
 
-# Обработчик выхода пользователя
+# Обработчик нового пользователя
 @bot.message_handler(content_types=['new_chat_members'])
 def handle_new_member(message):
     for user in message.new_chat_members:
-        if user.id == message.from_user.id:
-            # Обработать выход пользователя
-            phrase = random.choice(phrases_byby).format(user=user.first_name)
+        if user.id == bot.get_me().id:
+            # Отправить благодарственное сообщение
+            phrase = random.choice(phrases_thankful)
             bot.send_message(message.chat.id, phrase)
+        else:
+            # Обработать приветствие пользователя
+            phrase = random.choice(phrases_hello).format(user=user.first_name)
+            bot.send_message(message.chat.id, phrase)
+
+# Обработчик пользователя, который входит в чат по ссылке-приглашению
+@bot.message_handler(content_types=['member_invited'])
+def handle_invited_member(message):
+    user = message.invited_chat_member.first_name
+    phrase = random.choice(phrases_hello).format(user=user)
+    bot.send_message(message.chat.id, phrase)
 
 print("Бот успешно запущен!")
 
-bot.polling()
+bot.polling(none_stop=True)
